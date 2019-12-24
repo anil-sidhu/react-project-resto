59c2fe513e912cd89a10179746c87ad7736e0696
anil <webanilsidhu@gmail.com>
2019-12-24 22:54:09 +0530

    add search api

--- restaurant-app/src/components/RestaurantSearch.js
+++ restaurant-app/src/components/RestaurantSearch.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
 
 class RestaurantSearch extends Component {
+    constructor() {
+        super()
+        this.state = {
+            searchData: null,
+            noData:false,
+        }
+    }
+    search(key) {
+        console.warn(key)
+        fetch("http://localhost:3000/restaurant?q=" + key).then((data) => {
+            data.json().then((resp) => {
+                console.warn("resp", resp)
+                if(resp.length>0)
+                {
+                    this.setState({searchData:resp,noData:false})
+                }
+                else
+                {
+                    this.setState({noData:true,searchData:null})
+
+                }
+                
+            
+            })
+        })
+    }
     render() {
         return (
             <div>
                 <h1>Restaurant Search</h1>
-                
+                <input type="text" onChange={(event) => this.search(event.target.value)} />
+                <div>
+                    {
+                        this.state.searchData?
+                        <div>
+                            {
+                                this.state.searchData.map((item)=>
+                                <div className="search-row">{item.name}</div>
+                                    )
+                            }
+                        </div>
+                        :""
+                    }
+                    {
+                        this.state.noData?<h3>No Data Found</h3>:null
+                    }
+                </div>
+
             </div>
         );
     }
