85a01af7221d145d84f0a6dd5c8a7128bd3021ab
anil <webanilsidhu@gmail.com>
2020-02-05 22:30:56 +0530

    add search work

--- restaurant-app/src/components/RestaurantSearch.js
+++ restaurant-app/src/components/RestaurantSearch.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-
+import { Table,Form,Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faEdit,faTrash } from '@fortawesome/free-solid-svg-icons'
+import {
+    Link
+  } from 'react-router-dom'
 class RestaurantSearch extends Component {
     constructor() {
         super()
         this.state = {
             searchData: null,
             noData:false,
+            lastSearch:"",
         }
     }
     search(key) {
         console.warn(key)
+        this.setState({lastSearch:key})
         fetch("http://localhost:3000/restaurant?q=" + key).then((data) => {
             data.json().then((resp) => {
                 console.warn("resp", resp)
@@ -20,36 +27,70 @@ class RestaurantSearch extends Component {
                 else
                 {
                     this.setState({noData:true,searchData:null})
-
                 }
-                
-            
+            })
+        })
+    }
+    delete(id)
+    {
+        fetch('http://localhost:3000/restaurant/'+id,
+        {
+            method: "DELETE",
+            // headers:{
+            //     'Content-Type':'application/json'
+            // },
+        }).then((result)=>{
+            result.json().then((resp)=>{
+                alert("Restaurant has heen Delete")
+                this.search(this.state.lastSearch)
             })
         })
     }
     render() {
         return (
-            <div>
+            <Container>
                 <h1>Restaurant Search</h1>
-                <input type="text" onChange={(event) => this.search(event.target.value)} />
+               
+                <Form.Control type="text"  onChange={(event) => this.search(event.target.value)}   placeholder="Search restaurant" />
                 <div>
                     {
                         this.state.searchData?
                         <div>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Rating</th>
+                                        <th>Location</th>
+                                        <th>Operation</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
                             {
                                 this.state.searchData.map((item)=>
-                                <div className="search-row">{item.name}</div>
+                                <tr>
+                                                <td>{item.id}</td>
+                                                <td>{item.name}</td>
+                                                <td>{item.rating}</td>
+                                                <td>{item.address}</td>
+                                                <td><Link to={"/update/"+item.id}><FontAwesomeIcon icon={faEdit} color="orange" /> </Link>
+                                                <span onClick={()=>this.delete(item.id)}><FontAwesomeIcon icon={faTrash} color="red" /> </span>
+                                                </td>
+                                            </tr>
                                     )
                             }
+                            </tbody>
+                            </Table>
                         </div>
                         :""
                     }
                     {
                         this.state.noData?<h3>No Data Found</h3>:null
-                    }
+                    } 
                 </div>
 
-            </div>
+            </Container>
         );
     }
 }
